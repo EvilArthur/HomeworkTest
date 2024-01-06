bdf3211fc2cbce8002a2ad06336af6506116e020
Suleymanov Artur <l0tus0rb@yandex.ru>
2024-01-06 18:14:58 +0200

    WorkDone

--- src/main.php
+++ src/main.php
@@ -1,11 +1,42 @@
 <?php
 
 /**
- * @throws Exception for empty input
  * @param string $input
+ *
  * @return bool
+ * @throws Exception for empty input
  */
 function isParenthesisValid(string $input = ''): bool
 {
-	return true;
+	$openBrackets = '(<[';
+	$closeBrackets = ')>]';
+	$brackets = [];
+	if ($input === '')
+	{
+		throw new Exception("Empty input");
+	}
+	foreach (str_split($input) as $char)
+	{
+		if (strpos($closeBrackets, $char) !== false && empty($brackets))
+		{
+			return false;
+		}
+		if (
+			strpos($closeBrackets, $char) !== false
+			&& strpos($openBrackets, array_pop($brackets)) !== strpos($closeBrackets, $char)
+		)
+		{
+			return false;
+		}
+		if (strpos($openBrackets, $char) !== false)
+		{
+			$brackets[] = $char;
+		}
+	}
+	if (empty($brackets))
+	{
+		return true;
+	}
+
+	return false;
 }
--- tests/MainTests.php
+++ tests/MainTests.php
@@ -4,5 +4,31 @@ use PHPUnit\Framework\TestCase;
 
 class MainTests extends TestCase
 {
+	public function testTrivialCases()
+	{
+		$this->expectException(Exception::class);
+		$this->assertTrue(isParenthesisValid('Hello there'));
+		isParenthesisValid('');
+	}
 
+	public function testOneTypeBrackets()
+	{
+		$this->assertTrue(isParenthesisValid('Hello (there)'));
+		$this->assertFalse(isParenthesisValid('Hello (there'));
+		$this->assertFalse(isParenthesisValid('Hello )there('));
+	}
+
+	public function testTwoTypesBrackets()
+	{
+		$this->assertTrue(isParenthesisValid('Hello (th[e]re)'));
+		$this->assertFalse(isParenthesisValid('Hello (th[ere)'));
+		$this->assertFalse(isParenthesisValid('Hello (th[e)re]'));
+	}
+
+	public function testThreeTypesBrackets()
+	{
+		$this->assertTrue(isParenthesisValid('<Hello (there)>'));
+		$this->assertFalse(isParenthesisValid('Hello (there)>'));
+		$this->assertFalse(isParenthesisValid('Hello (th[e<)re]>'));
+	}
 }
