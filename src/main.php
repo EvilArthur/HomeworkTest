0bf8d52008c28ce7cc5255a8f14562e299b4b768
Suleymanov Artur <l0tus0rb@yandex.ru>
2024-01-06 17:42:07 +0200

    Init commit

---
+++ src/main.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @throws Exception for empty input
+ * @param string $input
+ * @return bool
+ */
+function isParenthesisValid(string $input = ''): bool
+{
+	return true;
+}
---
+++ tests/MainTests.php
@@ -0,0 +1,8 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class MainTests extends TestCase
+{
+
+}
